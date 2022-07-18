40f37e41a566e431b4cbbe2a510305c115e49d15
lavanya-sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-07-18 22:03:32 +0530

    Create maximum path sum from any node.java

---
+++ Trees/maximum path sum from any node.java
@@ -0,0 +1,35 @@
+class Solution
+{
+    //Function to return maximum path sum from any node in a tree.
+class Res{
+    int maxval=Integer.MIN_VALUE;
+}
+   int func(Node root,Res res){
+       if(root==null){ return 0;}
+       int l=func(root.left,res);
+      // System.out.print(" maximum value...."+res.maxval+" ");
+       int r=func(root.right,res);
+       res.maxval=Math.max(res.maxval,Math.max(l+root.data+r,Math.max(l+root.data,r+root.data)));
+     //  System.out.print("maxiimum value is"+res.maxval+"   ");
+      if(res.maxval<0){
+        return Math.max(Math.max(l,r),root.data);}//if all the values are negative...we are returning max among those
+       else{
+          if( (Math.max(l,r)+root.data)<0){
+              return 0;}
+          else{
+              return Math.max(l,r)+root.data;
+          }
+          
+       }
+     
+   }
+   
+    int findMaxSum(Node node)
+    {
+       Res res=new Res();
+       func(node,res);
+       return res.maxval;
+    }
+}
+// here why we are creating result class ...because ...we can update res value in every call...to make it possible...we are doing that.!!!
+//if any of the sum is negative.....we are not considering that subtree value......thats why we are returning zero.!!!!
