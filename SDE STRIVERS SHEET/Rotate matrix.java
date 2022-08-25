20e2f07d4718be1781394819a9e14d092a0a560a
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-08-25 10:00:10 +0530

    Create Rotate matrix.java

---
+++ SDE STRIVERS SHEET/Rotate matrix.java
@@ -0,0 +1,20 @@
+ public void rotate(int[][] matrix) {
+        int rows=matrix.length,cols=matrix[0].length;
+        for(int i=0;i<rows;i++){
+            for(int j=i;j<cols;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+       
+        for(int i=0;i<rows;i++){
+            int low=0,high=cols-1;
+            while(low<high){
+                int temp=matrix[i][low];
+                matrix[i][low]=matrix[i][high];
+                matrix[i][high]=temp;
+                low++;
+                high--;
+            }
+        }
