c6b280db6ebb2ab21417516c4c38fa2b65e15b7b
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-09-08 19:12:06 +0530

    Update Job sequencing.java

--- SDE SHEET PROBLEMS-GREEDY/Job sequencing.java
+++ SDE SHEET PROBLEMS-GREEDY/Job sequencing.java
@@ -5,22 +5,27 @@ class Solution
     {
         // Your code here
         int res[]=new int[2];
+        //sort the array in descending order based on profit
         Arrays.sort(arr,(a,b)->(b.profit-a.profit));
         int maxi=0;
+        //finding the max deadline to assign the jobs in particular slots
         for(int i=0;i<n;i++){
             if(arr[i].deadline>maxi){
                 maxi=arr[i].deadline;
             }
         }
+        //taking the array of max+1 size
         int result[]=new int[maxi+1];
         Arrays.fill(result,-1);
         int countjobs=0,profit=0;
+        // by traversing through the sorted array,we are placing the job based on deadline at the correct position
         for(int i=0;i<n;i++){
             for(int j=arr[i].deadline;j>0;j--){
                 if(result[j]==-1){
                     countjobs++;
                     profit+=arr[i].profit;
                     result[j]=i;
+                    //whenever we are placing the job at the correct position,we are breaking from the loop
                     break;
                 }
             }
