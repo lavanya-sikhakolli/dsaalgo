b14a8b72b793f76b49dc9e5805c2e0b38dec9329
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-09-20 16:13:34 +0530

    Update median of two sorted arrays using gap method.java

--- SDE SHEET PROBLEMS -BINARY SEARCH/median of two sorted arrays using gap method.java
+++ SDE SHEET PROBLEMS -BINARY SEARCH/median of two sorted arrays using gap method.java
@@ -2,20 +2,31 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m=nums1.length;
         int n=nums2.length;
-        int gap=(m+n)/2;
         double res=0.0;
+        
+        if(m>=1 && n>=1){
+        int gap=(m+n)/2;
         while(gap>0){
             int p1=0;
             int p2=gap;
             while(p2<m+n){
-                if(p2<m &&nums1[p1]>nums1[p2]){
-                    swap(nums1[p1],nums1[p2]);
+                if(p2<m &&p1<m&&nums1[p1]>nums1[p2]){
+                    //swap(nums1[p1],nums1[p2]);
+                    int temp=nums1[p1];
+                    nums1[p1]=nums1[p2];
+                    nums1[p2]=temp;
                 }
-                else if(p2>=m && nums1[p1]>nums2[p2-m]){
-                    swap(nums1[p1],nums2[p2-m]);
+                else if(p2>=m &&p1<m&& nums1[p1]>nums2[p2-m]){
+                    //swap(nums1[p1],nums2[p2-m]);
+                    int temp=nums1[p1];
+                    nums1[p1]=nums2[p2-m];
+                    nums2[p2-m]=temp;
                 }
                 else if(p2>=m && p1>=m && nums2[p1-m]>nums2[p2-m]){
-                    swap(nums2[p1-m],nums2[p2-m]);
+                    //swap(nums2[p1-m],nums2[p2-m]);
+                    int temp=nums2[p1-m];
+                    nums2[p1-m]=nums2[p2-m];
+                    nums2[p2-m]=temp;
                 }
                 p2++;
                 p1++;
@@ -49,11 +60,8 @@ class Solution {
                 return (double)nums2[m-mid];
             }
         }
+        }
         return res;
     }
-    public void swap(int a,int b){
-        int temp=a;
-        a=b;
-        b=temp;
-    }
 }
+   
