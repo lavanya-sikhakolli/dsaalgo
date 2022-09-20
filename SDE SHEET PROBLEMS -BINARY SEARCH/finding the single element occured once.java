3bb4c6b9e4e1ac885c69c03db66fbb4886a5aef9
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-09-20 12:32:52 +0530

    Update finding the single element occured once.java

--- SDE SHEET PROBLEMS -BINARY SEARCH/finding the single element occured once.java
+++ SDE SHEET PROBLEMS -BINARY SEARCH/finding the single element occured once.java
@@ -19,3 +19,38 @@ class Solution {
         return res;
     }
 }
+
+//by implementing using XOR
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int res=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            res=res^nums[i];
+        }
+        return res;
+    }
+}
+//by implementing using binary search
+class Solution{
+    public int singleNonDuplicate(int[] nums){
+        int low=0,high=nums.length-2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid%2==0){
+                if(nums[mid]!=num[mid+1]){
+                    high=mid-1;}
+                else{
+                    low=mid+1;}
+            }
+            else{
+                if(nums[mid]==nums[mid+1]){
+                    high=mid-1;}
+                else{
+                    low=mid+1;}
+            }
+        }
+            return nums[low];
+    }
+}
+        
