6ffebbcf777e0df6f150ad5bc0fc470a5b1053b5
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-09-21 22:59:28 +0530

    Update kth element of two sorted arrays.java

--- SDE SHEET PROBLEMS -BINARY SEARCH/kth element of two sorted arrays.java
+++ SDE SHEET PROBLEMS -BINARY SEARCH/kth element of two sorted arrays.java
@@ -1,43 +1,46 @@
 
-class Solution {
-   
-    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
-        int gap=(n+m)/2;
-        while(gap>0){
-            int p1=0,p2=gap;
-            while(p2<m+n){
-                if(p2<n &&p1<n&& arr1[p1]>arr1[p2]){
-                    //swap(arr1[p1],arr1[p2]);
-                    int temp=arr1[p1];
-                    arr1[p1]=arr1[p2];
-                    arr1[p2]=temp;
-                }
-                else if(p2>=n &&p1<n&&arr1[p1]>arr2[p2-n]){
-                   // swap(arr1[p1],arr2[p2-n]);
-                   int temp=arr1[p1];
-                   arr1[p1]=arr2[p2-n];
-                   arr2[p2-n]=temp;
-                }
-                else if(p2>=n && p1>=n && arr2[p1-n]>arr2[p2-n]){
-                    //swap(arr2[p1-n],arr2[p2-n]);
-                    int temp=arr2[p1-n];
-                    arr2[p1-n]=arr2[p2-n];
-                    arr2[p2-n]=temp;
-                }
-                p1++;
-                p2++;
+class Solution{
+public:
+
+    //time  : O(log(min(n,m)))
+    //space : O(1)
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        //if arr2 is smaller swap arr1 and arr2
+        if(m<n) return kthElement(arr2,arr1,m,n,k);
+        
+        //note we need kth element n is smaller
+        //n for arr1 ie smaller and m for arr2 ie greater
+        
+        //if k>m then taking 0 from arr1 and k from arr2 is not possible
+        //we have to take atleast k-m from arr1
+        //hence keep low as max(0,k-m)
+        int low = max(0,k-m);
+        //if k<n then taking all elements from arr1 is not possible
+        //we can pick atmost k from arr1 and 0 from arr2
+        //hence keep high as min(k,n)
+        int high = min(n,k);
+        
+        while(low <= high){
+            int cut1 = low + high >> 1;
+            int cut2 = k - cut1;
+            
+            int l1 = (cut1==0) ? INT_MIN : arr1[cut1-1];
+            int l2 = (cut2==0) ? INT_MIN : arr2[cut2-1];
+            
+            int r1 = (cut1==n) ? INT_MAX : arr1[cut1];
+            int r2 = (cut2==m) ? INT_MAX : arr2[cut2];
+            
+            //valid cut
+            if(l1 <= r2 && l2 <= r1){
+                //ie kth element 
+                return max(l1,l2);
             }
-            gap=gap/2;
+            //reduce l1 -> cut move to left side in arr1
+            else if(l1 > r2) { high = cut1 - 1; }
+            //l2 > r1 increase r1 -> cut move to right side in arr1
+            else { low = cut1 + 1; }
         }
-        if(k-1<n){
-            return (long)arr1[k-1];
-        }
-        else if(k-1==n){
-            return (long)arr2[0];
-        }
-        else if(k-1>n){
-            return (long)arr2[(k-1)-n];
-        }
-       return (long)1; 
     }
-}
+};
+
