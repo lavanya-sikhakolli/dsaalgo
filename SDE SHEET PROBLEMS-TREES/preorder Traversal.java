94cc201fb0567bb359ce09198aadbc28054f90cb
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-10-10 23:37:46 +0530

    Update preorder Traversal.java

--- SDE SHEET PROBLEMS-TREES/preorder Traversal.java
+++ SDE SHEET PROBLEMS-TREES/preorder Traversal.java
@@ -16,3 +16,32 @@ class Solution {
         return res;
     }
 }
+//Using Iterative
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<Integer>();
+        if(root==null){
+            return res;
+        }
+        Stack<TreeNode> s=new Stack<TreeNode>();
+        TreeNode curr=root;
+        TreeNode prev=null;
+        while(curr!=null ||s.isEmpty()==false){
+            while(curr!=null){
+                res.add(curr.val);
+                s.push(curr);
+                curr=curr.left;
+            }
+            curr=s.peek();
+            if(curr.right!=null &&curr.right!=prev){
+                curr=curr.right;
+            }
+            else{
+                s.pop();
+                prev=curr;
+                curr=null;
+            }
+        }
+        return res;
+    }
+}
