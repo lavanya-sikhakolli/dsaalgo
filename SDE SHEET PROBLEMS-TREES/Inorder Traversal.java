1382adf5e215cbd015e3ed3a5ff93cf72af020a0
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-10-10 16:43:59 +0530

    Update Inorder Traversal.java

--- SDE SHEET PROBLEMS-TREES/Inorder Traversal.java
+++ SDE SHEET PROBLEMS-TREES/Inorder Traversal.java
@@ -17,3 +17,28 @@ class Solution {
         return res;
     }
 }
+
+
+//Inorder Using iterative approach....called as Morris Inorder Traversal
+class Solution {
+   
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+       Stack<TreeNode> s=new Stack<TreeNode>();
+        TreeNode curr=root;
+        while(curr!=null || s.isEmpty()==false){
+            while(curr!=null){
+                s.push(curr);
+                curr=curr.left;
+            }
+            curr=s.peek();
+            s.pop();
+            res.add(curr.val);
+            curr=curr.right;
+        }
+        return res;
+    }
+}
