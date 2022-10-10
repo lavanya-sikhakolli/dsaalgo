c02c10e54733cc6ab45f884965c7fe3dc5ca53c3
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-10-10 23:49:47 +0530

    Update postOrder traversal.java

--- SDE SHEET PROBLEMS-TREES/postOrder traversal.java
+++ SDE SHEET PROBLEMS-TREES/postOrder traversal.java
@@ -16,3 +16,32 @@ class Solution {
         return res;
     }
 }
+//Using Iteration
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<Integer>();
+        if(root==null){
+            return res;
+        }
+        Stack<TreeNode> s=new Stack<TreeNode>();
+        TreeNode curr=root;
+        TreeNode prev=null;
+        while(curr!=null ||s.isEmpty()==false){
+            while(curr!=null){
+                s.push(curr);
+                curr=curr.left;
+            }
+            curr=s.peek();
+            if(curr.right!=null && curr.right!=prev){
+                curr=curr.right;
+            }
+            else{
+                s.pop();
+                res.add(curr.val);
+                prev=curr;
+                curr=null;
+            }
+        }
+        return res;
+    }
+}
