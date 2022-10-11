25a8f46c52e7e1fe7b0f099d104ec75895227fb0
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-10-11 12:48:16 +0530

    Update Level order traversal using list of list.java

--- SDE SHEET PROBLEMS-TREES/Level order traversal using list of list.java
+++ SDE SHEET PROBLEMS-TREES/Level order traversal using list of list.java
@@ -24,3 +24,41 @@ class Solution {
         return res;
     }
 }
+//Zig-Zag traversal
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        Stack<Integer> s=new Stack<>();
+        boolean flag=false;
+        q.add(root);
+        while(q.isEmpty()==false){
+            int count=q.size();
+            List<Integer> res1=new ArrayList<>();
+            for(int i=0;i<count;i++){
+                TreeNode curr=q.poll();
+            if(flag==true){
+               s.push(curr.val);
+            }
+         else{
+                res1.add(curr.val);}
+                 if(curr.left!=null){ q.add(curr.left);}
+                 if(curr.right!=null){ q.add(curr.right);}
+                
+            }
+                //res.add(res1);
+            
+            if(flag==true){
+                while(s.isEmpty()==false){
+                res1.add(s.pop());}
+            }
+                flag=!flag;
+            res.add(res1);
+         
+        }
+        return res;
+    }
+}
