27a09a3f6de8b9946ef8c06dcfcb2e5c40b4e55c
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-10-11 20:43:41 +0530

    Update Views of binary tree.java

--- SDE SHEET PROBLEMS-TREES/Views of binary tree.java
+++ SDE SHEET PROBLEMS-TREES/Views of binary tree.java
@@ -23,3 +23,46 @@ class Tree
       return res;    
     }
 }
+//TOP VIEW OF A BINARY TREE
+class Pair{
+    Node node;
+    int hd;
+    Pair(Node n,int h){
+        node=n;
+        hd=h;
+    }
+}
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        // add your code
+        ArrayList<Integer> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<Pair> q=new LinkedList<>();
+        Map<Integer,Integer> mp=new TreeMap();
+        q.add(new Pair(root,0));
+        while(q.isEmpty()==false){
+            Pair p=q.poll();
+            Node curr=p.node;
+            int hd=p.hd;
+            if(!mp.containsKey(hd)){
+                mp.put(hd,curr.data);
+            }
+            if(curr.left!=null){
+                q.add(new Pair(curr.left,hd-1));
+            }
+            if(curr.right!=null){
+                q.add(new Pair(curr.right,hd+1));
+            }
+        }
+        for(Map.Entry<Integer,Integer> p:mp.entrySet()){
+            res.add(p.getValue());
+        }
+        return res;
+    }
+}
