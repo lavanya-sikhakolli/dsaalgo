52af5dbf50199ad28c15b20218e8c2b96c3e2e50
Lavanya Sikhakolli <109140008+lavanya-sikhakolli@users.noreply.github.com>
2022-11-14 12:20:14 +0530

    Update Search in a sorted and rotated array.java

--- SDE SHEET PROBLEMS -BINARY SEARCH/Search in a sorted and rotated array.java
+++ SDE SHEET PROBLEMS -BINARY SEARCH/Search in a sorted and rotated array.java
@@ -8,15 +8,20 @@ class Solution {
             int mid=low+(high-low)/2;
             if(nums[mid]==target){ return mid;}
             else if(nums[low]<=nums[mid]){
+                //left subarray is sorted
                 if(target>=nums[low]&&target<nums[mid]){
+                    //if the element to be search in the array is lie in that range 
                     high=mid-1;
                 }
                 else{
+                    //if not....check with the other range
                     low=mid+1;
                 }
             }
             else{
+                //right subarray is sorted
                 if(target>nums[mid]&& target<=nums[high]){
+                    //if the element to be search in the array is lie in that range
                     low=mid+1;
                 }
                 else{
